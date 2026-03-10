113c5f9b708746bb107ba6f7512755fad35be7c1
DimenCoder <renangameslinha8@gmail.com>
2026-03-10 17:25:21 -0300

    Bump version from 1.0.0 to 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cockroach",
-    version="1.0.0",
+    version="1.0.1",
     description="Simple",
     author="Inferno-God1001",
     packages=find_packages(),
